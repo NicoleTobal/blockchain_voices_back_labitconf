03fd4062d82cc297394926bfbf97e23a31b09a0a
Nicole Tobal <nickytobal@gmail.com>
2018-12-02 15:26:48 -0300

    Fix in cors

--- app.js
+++ app.js
@@ -30,14 +30,18 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Access-Control-Allow-Origin');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token,Access-Control-Allow-Origin');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
+  if ('OPTIONS' === req.method) {
+    res.send('200');
+  } else {
+    // Pass to next layer of middleware
+    next();
+  } 
 });
 
 app.use((req, res, next) => {
